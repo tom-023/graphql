506958de8aa09fe608f6035255484b57f525ed49
watanabe atsushi <nabesan07610057@gmail.com>
2019-12-11 11:58:35 +0900

    データの紐付け

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,4 +10,11 @@ Types::QueryType = GraphQL::ObjectType.define do
       "Hello World!"
     }
   end
+
+  field :blog, !Types::BlogType do
+    resolve ->(_obj, _args, ctx) {
+      ctx[:blog]
+    }
+  end
+
 end
